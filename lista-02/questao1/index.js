1aa65b1e5c8f87def716d2c65744fbee83e71876
yurepires <contato.yurepires@gmail.com>
2024-11-19 16:37:53 -0300

    Arrow function utilizada

--- lista-02/questao1/index.js
+++ lista-02/questao1/index.js
@@ -2,4 +2,10 @@ import calc from './vetores.js'
 
 var vetor = [3, 5, 7, 10, 12]
 
-console.log(calc.somaVetor(vetor, calc.dobraVetor))
+console.log(calc.somaVetor(vetor, (vetor) => {
+    let vetorDuplicado = []
+    for(let i = 0; i < vetor.length; i++){
+        vetorDuplicado[i] = vetor[i] * 2
+    }
+    return vetorDuplicado
+}))
