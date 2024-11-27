502be7c9fbb0066fa9cbbba5c396d096c7392022
yurepires <contato.yurepires@gmail.com>
2024-11-27 16:50:55 -0300

    mensagem adicionada!

--- aula-25-11-2024/index.js
+++ aula-25-11-2024/index.js
@@ -2,4 +2,10 @@ import express from 'express'
 
 const app = express()
 
-app.listen(3200)
+app.get('/', (req, res) => {
+    res.send('Imsoolazy is the better than anyone!')
+})
+
+app.listen(3200, () => {
+    console.log("Example app listening on port 3200")
+})
