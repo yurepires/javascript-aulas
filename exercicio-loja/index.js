8db8cac2cc34043fc5675eb7d490fcb425a7322c
20232ADS.CNT0019 <contato.yurepires@gmail.com>
2024-12-02 22:05:09 -0300

    Aprendemos a usar senFile e importar arquivos estáticos para o express e usar o nodemon

--- exercicio-loja/index.js
+++ exercicio-loja/index.js
@@ -6,16 +6,27 @@ import sobre from './html/sobre.js'
 const app = express()
 const port = 8010
 
+// Importar arquivos
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+//dizer para o node a pasta de conteudos estaticos
+//public é só o nome da pasta, não sendo obrigatório usar sempre public
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 app.get('/', (req, res) => {
-    res.send(home.function())
+    res.sendFile(__dirname+'/html-professor/index.html')
 })
 
 app.get('/produtos', (req, res) => {
-    res.send(produtos.function())
+    res.sendFile(__dirname+'/html-professor/produtos.html')
 })
 
 app.get('/sobre', (req, res) => {
-    res.send(sobre.function())
+    res.sendFile(__dirname+'/html-professor/sobre.html')
 })
 
 app.listen(port, () => {
